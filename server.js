82e47cb93f6b9f9ce70cd3b6b24f405ce6d095ce
Neville Mehta <ndesk1900@gmail.com>
2017-07-30 22:40:14 -0700

    pre-build and send to browser

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
-var index = require('./src/index.marko');
 
-app.get('/', function (req, res) {
-  //res.marko(index, {});
-});
+require('marko/node-require'); // Allow Node.js to require and load `.marko` files
+
+var express = require('express');
+var markoExpress = require('marko/express');
+var serveStatic = require('serve-static');
+//var template = require('./src/index.marko');
+
+var app = express();
+
+app.use(markoExpress()); //enable res.marko(template, data)
+
+// Allow all of the generated files under "static" to be served up by Express
+app.use('/', serveStatic(__dirname + '/static'));
+
+/*app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.marko(template, {});
+});*/
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
